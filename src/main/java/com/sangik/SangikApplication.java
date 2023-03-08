62aebe48b1edfc7ba4ea4ee7d7c61bbb078dfdc5
SangIkCheon <crackers0424@gmail.com>
2023-03-08 22:09:19 +0900

    초기설정

---
+++ src/main/java/com/sangik/SangikApplication.java
@@ -0,0 +1,13 @@
+package com.sangik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SangikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sangik/SangikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
